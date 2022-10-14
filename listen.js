c5278bc634f2d274c0776cae734ed9826a538aa4
Ryan Foo <ryanfoocj@gmail.com>
2022-10-14 10:00:05 +0100

    Heroku configs

--- listen.js
+++ listen.js
@@ -1,6 +1,5 @@
-//const { PORT = 9090 } = process.env;
-const express = require("express");
+const { PORT = 9090 } = process.env;
+
 const app = express();
-const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
