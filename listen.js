a03b0900c642c78fb9cbaa27b90271058d84a4b2
Ryan Foo <ryanfoocj@gmail.com>
2022-10-14 09:56:50 +0100

    Added port to listen.js

--- listen.js
+++ listen.js
@@ -1,4 +1,6 @@
-const { PORT = 9090 } = process.env;
-const app = require("./app");
+//const { PORT = 9090 } = process.env;
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
